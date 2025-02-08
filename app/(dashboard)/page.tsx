7ecf3bca59eff8f69ebaa003efb7664a3f079823
Zulu <juanpablozuluaga.0311@gmail.com>
2025-02-08 08:22:06 -0500

    Paginación retirada de más lugares conflictivos.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,13 +6,12 @@ import { AuthRedirect } from './authredirect';
 export default async function ProductsPage({
   searchParams
 }: {
-  searchParams: Promise<{ q: string; offset: string }>;
+  searchParams: Promise<{ q: string }>;
 }) {
   const searchParamsData = await searchParams;
   const search = searchParamsData.q ?? '';
-  const offset = parseInt(searchParamsData.offset ?? '0');
 
-  const { products, newOffset, totalProducts } = await getProducts(search, offset);
+  const { products } = await getProducts(search);
 
   return (
     <>
@@ -23,11 +22,7 @@ export default async function ProductsPage({
         </TabsList>
 
         <TabsContent value="all">
-          <ProductsClient
-            products={products}
-            newOffset={newOffset ?? 0}
-            totalProducts={totalProducts}
-          />
+          <ProductsClient products={products} />
         </TabsContent>
       </Tabs>
     </>
